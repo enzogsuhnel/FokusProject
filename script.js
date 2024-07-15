27e6f58270d071a7745e2410656f4e01fd4a5375
enzogsuhnel <enzogsuhnel@gmail.com>
2024-07-15 15:11:33 -0300

    Added: (Tittle types, images types [problem]) + generalized function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focusButton = document.querySelector('.app__card-button--foco');
 const shortButton = document.querySelector('.app__card-button--curto');
 const longButton = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const tittle = document.querySelector('.app__title');
 
 focusButton.addEventListener('click', () => {
     alterarContexto('foco');
@@ -19,7 +20,21 @@ longButton.addEventListener('click', () => {
 function alterarContexto(contexto){
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`)
-}
+    switch(contexto) {
+        case 'foco':
+            tittle.innerHTML = `Otimize sua produtividade,<br>
+                    <strong class="app__title-strong">mergulhe no que importa.</strong>`
+        break;
+        case 'descanso-curto':
+            tittle.innerHTML = `Que tal dar uma respirada?<br>
+                  <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+        break;
+        case 'descanso-longo':
+            tittle.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa!</strong>`
+        default:
+            break;    
+}}
 
 /*const html = document.querySelector('html');
 const focusButton = document.querySelector('.app__card-button--foco');
